6090ece302bb2596f6ce9ba3c2c3000fc980d122
Sushil Shrestha <sushil771@gmail.com>
2023-03-11 22:16:17 -0800

    Initial Commit

---
+++ Git_demo/Git_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_demoApp.swift
+//  Git_demo
+//
+//  Created by Sushil Shrestha on 3/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Git_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
